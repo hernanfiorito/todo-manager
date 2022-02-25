8f4c3b7771be9e7649a342e11c4441cb8b39b5bb
Hernan Fiorito <h.d.fiorito@gmail.com>
2022-02-24 23:48:42 -0300

    Initial commit

---
+++ src/test/java/com/example/todoManager/TodoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
